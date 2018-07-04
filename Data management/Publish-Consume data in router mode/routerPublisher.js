ea8d83474bcf6716aa11fcfb8fc546501e1d7bac
Live Objects <liveobjects.contact@orange.com>
2018-07-04 23:59:37 +0200

    Create routerPublisher.js

---
+++ Data management/Publish-Consume data in router mode/routerPublisher.js
@@ -0,0 +1,33 @@
+/**
+ * Application connects to LO and publish messages to the router.
+ *
+ * Messages can be consume from a "routerConsumer", but your consumer application has to be running before you send messages from the publisher
+ * You can also consume message in fifo mode (use the MqttFifoSample (FifoConsumer.java) after having declared a binding in Live Objects.
+ * In that case your message can be consumed
+ *
+ */
+var mqtt =require('mqtt.js');
+const url = "mqtt://liveobjects.orange-business.com:1883";
+const apiKey = "enter your api key here";     // ENTER API KEY HERE
+const USERNAME  = "payload+bridge";
+const mqttTopic = "router/~event/v1/data/samples/new";
+const CLIENT_ID = "myClientId";
+
+var message = "Message published in router mode from routerPublisher.js";
+
+console.log(message);
+var client = mqtt.connect(url, {username:USERNAME,clientId:CLIENT_ID, password:apiKey, keepAlive:30})
+
+client.on("connect", function() {
+
+  console.log("MQTT::Connected");
+  client.publish(mqttTopic, message);
+  console.log("MQTT::Infos published");
+  client.end();
+  console.log("MQTT::Disconnected");
+
+});
+
+client.on("error", function(err) {
+  console.log("MQTT::Error from client --> ", err);
+});
